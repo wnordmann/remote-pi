d7464dc51a01285f42c38d1e2d9725cf83bf17f2
willie nordmann <william.nordmann@gmail.com>
2018-04-08 18:12:10 -0500

    Increasing blnk time

--- BlinkingLED.js
+++ BlinkingLED.js
@@ -1,6 +1,6 @@
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
-var blinkInterval = setInterval(blinkLED, 500); //run the blinkLED function every 250ms
+var blinkInterval = setInterval(blinkLED, 1000); //run the blinkLED function every 250ms
 
 function blinkLED() { //function to start blinking
   if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
@@ -16,4 +16,4 @@ function endBlink() { //function to stop blinking
   LED.unexport(); // Unexport GPIO to free resources
 }
 
-setTimeout(endBlink, 5000); //stop blinking after 5 seconds
+setTimeout(endBlink, 10000); //stop blinking after 5 seconds
